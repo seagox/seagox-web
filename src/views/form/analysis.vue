1a9f007ada2f77688355622dc901640b3eafa3fa
jc_xzh <15160037796@163.com>
2023-05-10 11:23:42 +0800

    123

--- src/views/form/analysis.vue
+++ src/views/form/analysis.vue
@@ -182,7 +182,7 @@ export default {
 				} else if (item.type === 'quick') {
 					this.queryQuickAccess(item)
 				} else if (item.type === 'chart') {
-					if(item.dataSourceType === 'dataModel') {
+					if(item.dataSourceType === 'dataModel' || item.dataSourceType === 'view') {
 						this.$nextTick(() => {
 							var filterDataSql = ''
 							for(let i=0;i<item.filterData.length;i++) {
